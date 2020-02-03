75b09482e69f8c54713dda313fe4861423cf8887
Drake Wilcox and Patrick Kille <patrick@>
2020-02-03 15:53:21 -0800

    add functions to main.js to output the results of functions in journal.js.

--- src/js/journal.js
+++ src/js/journal.js
@@ -6,31 +6,42 @@ export function Entry(title, body) {
 Entry.prototype.numberOfWords = function(){
   var titleWords = this.title.split(" ");
   var bodyWords = this.body.split(" ");
-  totalWords = titleWords.length + bodyWords.length;
+  var totalWords = titleWords.length + bodyWords.length;
   return totalWords;
-}
+};
+Entry.prototype.getTeaser = function(){
+  var bodyWords = this.body.split(" ");
+  var teaser = bodyWords.splice(0,8);
+  for (var i = 0; i < teaser.length; i++) {
+    if(teaser[i].includes(".")) {
+      teaser.splice(i + 1);
+    }
+  }
+
+  return teaser.join(" ")
+};
 
 Entry.prototype.vowels = function(){
   var letters = this.title.split("").concat(this.body.split(""));
   var vowelCount = 0;
-  var vowelArray = ["a", "e", "i" , "o", "u", "A", "E", "I", "O", "U"]
+  var vowelArray = ["a", "e", "i" , "o", "u", "A", "E", "I", "O", "U"];
   letters.forEach(letter => {
-      if (vowelArray.includes(letter)) {
-        vowelCount++;
-      }
-    });
+    if (vowelArray.includes(letter)) {
+      vowelCount++;
+    }
+  });
   return vowelCount;
-}
+};
 
 Entry.prototype.cons = function(){
   var letters = this.title.concat(this.body);
   var letterArray = letters.match(/[A-Za-z]/g);
   var consCount = 0;
-  var vowelArray = ["a", "e", "i" , "o", "u", "A", "E", "I", "O", "U"]
+  var vowelArray = ["a", "e", "i" , "o", "u", "A", "E", "I", "O", "U"];
   letterArray.forEach(letter => {
     if (vowelArray.includes(letter) === false) {
       consCount++;
     }
   });
   return consCount;
-}
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-import '.css/styles.css';
+import './../css/bootstrap.css';
+import './../css/styles.css';
+import { Entry } from './journal.js';
+
+$(document).ready(function(){
+
+  $("#newSubmission").click(function(){
+   
+    var title = $("#title").val();
+    var body = $("#entry").val();
+    var newEntry = new Entry(title, body);
+   
+    var wordCount = newEntry.numberOfWords();
+    var vowelCount = newEntry.vowels();
+    var conCount = newEntry.cons();
+    var teaserText = newEntry.getTeaser();
+
+    $("#teaserOutput").html("Teaser: " + teaserText);
+    $("#wordCountOutput").text("Word Count: " + wordCount);
+    $("#vowelCountOutput").text("Vowel Count: " + vowelCount);
+    $("#conCountOutput").text("Consonants Count: " + conCount);
+
+  });
+
+}); 
